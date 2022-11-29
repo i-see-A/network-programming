2ab0fb769bc8cb86aef0810dcad9a5730b85fab8
jungsiroo <sh2298@naver.com>
2022-11-29 23:33:26 +0900

    Get Full screen size

--- AtariBrickBreaker/src/AtariClientMain.java
+++ AtariBrickBreaker/src/AtariClientMain.java
@@ -20,7 +20,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class AtariClientMain{ // 게임타이틀화면
+public class AtariClientMain { // 게임타이틀화면
 
 	private JFrame frame;
 	private JTextField txtUserName;
@@ -61,91 +61,92 @@ public class AtariClientMain{ // 게임타이틀화면
 		try {
 			Path currentPath = Paths.get(System.getProperty("user.dir"));
 
-			Path dohyeonFontPath = Paths.get(currentPath.toString().replace("AtariBrickBreaker", ""), "AtariBrickBreaker", "assets", "fonts",
+			Path dohyeonFontPath = Paths.get(currentPath.toString().replace("AtariBrickBreaker", ""),
+					"AtariBrickBreaker", "assets", "fonts",
 					"BMDOHYEON.ttf");
-			Path cookieRunFontPath = Paths.get(currentPath.toString().replace("AtariBrickBreaker", ""), "AtariBrickBreaker", "assets", "fonts",
+			Path cookieRunFontPath = Paths.get(currentPath.toString().replace("AtariBrickBreaker", ""),
+					"AtariBrickBreaker", "assets", "fonts",
 					"CookieRun_Bold.ttf");
 
 			BM_DOHYEON_BOLD = Font.createFont(Font.TRUETYPE_FONT, dohyeonFontPath.toFile())
 					.deriveFont(Font.BOLD, 32f);
 			COOKIE_RUN_BOLD = Font.createFont(Font.TRUETYPE_FONT, new File(cookieRunFontPath.toString()))
 					.deriveFont(Font.PLAIN, 18f);
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-
 			// register the font
 			ge.registerFont(BM_DOHYEON_BOLD);
 			ge.registerFont(COOKIE_RUN_BOLD);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		frame = new JFrame();
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setBackground(new Color(255, 255, 255));
 		frame.setBackground(new Color(255, 255, 255));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(gd.getDisplayMode().getWidth(), gd.getDisplayMode().getHeight());
 		frame.setResizable(false);
-		gd.setFullScreenWindow(frame); //전체화면
+		// gd.setFullScreenWindow(frame); // 전체화면
 		frame.getContentPane().setLayout(new BorderLayout());
-		
-		gameTitle gt = new gameTitle(); //게임 제목, 버튼들 들어있는 Jpanel
+
+		gameTitle gt = new gameTitle(); // 게임 제목, 버튼들 들어있는 Jpanel
 		frame.getContentPane().add(gt, BorderLayout.CENTER);
 	}
-	
-	class gameTitle extends JPanel{
+
+	class gameTitle extends JPanel {
 		Font BM_DOHYEON_BOLD = new Font("배달의민족 도현", Font.BOLD, 18);
 		Font COOKIE_RUN_BOLD = new Font("CookieRun_Bold", Font.BOLD, 18);
 		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-		int w = (int)dimension.getWidth();
-		int h = (int)dimension.getHeight();
-		
+		int w = (int) dimension.getWidth();
+		int h = (int) dimension.getHeight();
+
 		public gameTitle() {
-			//JPanel 게임 제목과, 이름 입력, 버튼 넣을
+			// JPanel 게임 제목과, 이름 입력, 버튼 넣을
 			setBackground(Color.white);
 			setLayout(null);
 			setVisible(true);
-			setSize(800,800);
+			setSize(800, 800);
 
 			JLabel lblTitle = new JLabel("ATARI BRICK BREAKER");
-			lblTitle.setBounds((int)(0.5*w - 300),(int)(0.5*h-200),600,60);
+			lblTitle.setBounds((int) (0.5 * w - 300), (int) (0.5 * h - 200), 600, 60);
 			lblTitle.setForeground(new Color(0, 0, 0));
 			lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-			lblTitle.setFont(BM_DOHYEON_BOLD.deriveFont(Font.PLAIN,52f));
+			lblTitle.setFont(BM_DOHYEON_BOLD.deriveFont(Font.PLAIN, 52f));
 			add(lblTitle);
-			
+
 			JLabel lblPhrase = new JLabel("Enter your name below");
 			lblPhrase.setForeground(new Color(0, 128, 255));
 			lblPhrase.setFont(BM_DOHYEON_BOLD.deriveFont(Font.PLAIN, 18f));
 			lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-			lblPhrase.setBounds((int)(0.5*w-109),(int)(0.5*h-50),250,48);
+			lblPhrase.setBounds((int) (0.5 * w - 109), (int) (0.5 * h - 50), 250, 48);
 			add(lblPhrase);
 
 			// UserName 입력
 			txtUserName = new JTextField();
-			txtUserName.setBounds((int)(0.5*w - 150),(int)(0.5*h),300,50);
+			txtUserName.setBounds((int) (0.5 * w - 150), (int) (0.5 * h), 300, 50);
 			txtUserName.setColumns(10);
 			txtUserName.setFont(BM_DOHYEON_BOLD);
 			add(txtUserName);
 
 			// single play
 			JButton btnSingle = new JButton("Single");
-			btnSingle.setBounds((int)(0.5*w - 200),(int)(0.5*h+150), 150, 40);
+			btnSingle.setBounds((int) (0.5 * w - 200), (int) (0.5 * h + 150), 150, 40);
 			btnSingle.setFont(COOKIE_RUN_BOLD.deriveFont(Font.PLAIN, 24f));
 			btnSingle.setBackground(new Color(192, 192, 192));
 			add(btnSingle);
-			
+
 			JButton btnMulti = new JButton("Multi");
 			btnMulti.setFont(COOKIE_RUN_BOLD.deriveFont(Font.PLAIN, 24f));
 			btnMulti.setBackground(Color.LIGHT_GRAY);
-			btnMulti.setBounds((int)(0.5*w + 50),(int)(0.5*h+150), 150, 40);
+			btnMulti.setBounds((int) (0.5 * w + 50), (int) (0.5 * h + 150), 150, 40);
 			add(btnMulti);
-			
+
 			Myaction action = new Myaction();
 			btnSingle.addActionListener(action);
 			btnMulti.addActionListener(action);
 		}
 	}
-	
+
 	class Myaction implements ActionListener {
 
 		@Override
